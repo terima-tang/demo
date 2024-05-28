c58f1f7eff67b0d849ad65ce2047af379fc4736b
tangyun <tangyun@sinosoft.com.cn>
2024-05-28 17:54:18 +0800

    添加了新的特性

---
+++ src/main/java/com/example/demo/son.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+
+import entity.test;
+
+public class son extends test {
+    @Override
+    public void print() {
+        super.print();
+
+    }
+
+    @Override
+    public String print_test() {
+        return null;
+    }
+}
--- src/main/java/entity/test.java
+++ src/main/java/entity/test.java
@@ -1,12 +1,25 @@
 package entity;
 
 public class test {
+
+
+    public Object obj;
     int a1;
     protected int a2;
 
     private int a3;
 
     public int a4;
+
+    public test() {
+    }
+
+    public test(int a1, int a2, int a3, int a4) {
+        this.a1 = a1;
+        this.a2 = a2;
+        this.a3 = a3;
+        this.a4 = a4;
+    }
 
     public int getA1() {
         return a1;
@@ -39,4 +52,16 @@ public class test {
     public void setA4(int a4) {
         this.a4 = a4;
     }
+
+    public void print(){
+        System.out.println("a1:"+a1+"a2:"+a2);
+
+    }
+
+    public Ob print_test(){
+        System.out.println("123");
+        return null;
+    }
+
+
 }
